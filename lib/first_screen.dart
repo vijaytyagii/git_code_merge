7aa0700605b87e4a91beaeac0bd13f6ac689d795
vijaytyagii <vijaytyagi2002@gmail.com>
2026-06-10 21:45:15 +0530

    first screen commit

---
+++ lib/first_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
