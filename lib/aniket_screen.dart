197531ab03317f7a547383bb5fbe70534682197e
vijaytyagii <vijaytyagi2002@gmail.com>
2026-06-10 21:49:15 +0530

    first screen commit

---
+++ lib/aniket_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AniketScreen extends StatefulWidget {
+  const AniketScreen({super.key});
+
+  @override
+  State<AniketScreen> createState() => _AniketScreenState();
+}
+
+class _AniketScreenState extends State<AniketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
