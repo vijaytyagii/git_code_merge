152341a423e29e12d48ca68bbb8a0bf2c6c84afe
vijaytyagii <vijaytyagi2002@gmail.com>
2026-06-10 21:47:40 +0530

    first screen commit

---
+++ lib/vijay_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class VijayScreen extends StatefulWidget {
+  const VijayScreen({super.key});
+
+  @override
+  State<VijayScreen> createState() => _VijayScreenState();
+}
+
+class _VijayScreenState extends State<VijayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
